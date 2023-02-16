b0c5b90abeb46f3fbd412488011ce30f8702ae24
zhuzhen <2461705356@qq.com>
2023-02-16 22:30:55 +0800

    20230216第一次上传

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+// console.log('hello world' + '世界');
+// var a = 1, b;
+// console.log(typeof (a), b, null);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE, Date());
+// console.log(Boolean('1'));
+
+var span = document.querySelectorAll('.icon-cover');
+for (var i = 0, len = span.length; i < len; i++) {console.log(span[i].querySelector('span').click());
+}
+
