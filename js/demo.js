11e2338d3694448be00b88b6edecef9f132d5d12
zhuzhen <2461705356@qq.com>
2023-02-19 00:36:41 +0800

    新增电梯导航

--- js/demo.js
+++ js/demo.js
@@ -5,7 +5,9 @@
 // console.log(Number.MIN_VALUE, Date());
 // console.log(Boolean('1'));
 
-var span = document.querySelectorAll('.icon-cover');
-for (var i = 0, len = span.length; i < len; i++) {console.log(span[i].querySelector('span').click());
-}
+console.info('本页面由 skyblue 开发维护  ',Date())
 
+// 冒泡排序
+function bubbleSort(arr) { for (let i = 0; i < arr.length; i++) { for (let j = 0; j < arr.length - i - 1; j++) { if (arr[j] > arr[j + 1]) { let temp = arr[j]; arr[j] = arr[j + 1]; arr[j + 1] = temp; } } } return arr; }
+
+console.info(bubbleSort([6,3,5,8,4,2]))
